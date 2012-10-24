1a43f48bdfe32934babbe7550a9c90ab11bcd989
Marcus Derek <xxlifewirexx@gmail.com>
2012-10-24 21:35:21 +0800

    Removed ClientCommands, revised to commands

---
+++ SSLClient/commands.h
@@ -0,0 +1,23 @@
+//
+//  commands.h
+//
+//  Created by Lion User on 14/10/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef COMMANDS_H
+#define COMMANDS_H
+
+char* get_IntroMsg();
+char* get_HelpList();
+int verifyUserCommand();
+char* registerNewAccount();
+char* loginToAccount();
+char* addFile();
+char* deleteFile();
+char* concatSentence(int command, char* sentence1, char* sentence2);
+
+
+
+
+#endif
