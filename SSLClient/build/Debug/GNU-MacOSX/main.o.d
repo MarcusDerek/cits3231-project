c954f506c2241dbb604749d46add47bc14b9549d
Marcus Derek <xxlifewirexx@gmail.com>
2012-10-24 19:34:09 +0800

    Base Code for SSL Server

---
+++ SSLClient/build/Debug/GNU-MacOSX/main.o.d
@@ -0,0 +1,10 @@
+build/Debug/GNU-MacOSX/main.o: main.c \
+  ../openssl-1.0.1c/include/openssl/bio.h \
+  ../openssl-1.0.1c/include/openssl/ssl.h \
+  ../openssl-1.0.1c/include/openssl/err.h
+
+../openssl-1.0.1c/include/openssl/bio.h:
+
+../openssl-1.0.1c/include/openssl/ssl.h:
+
+../openssl-1.0.1c/include/openssl/err.h:
