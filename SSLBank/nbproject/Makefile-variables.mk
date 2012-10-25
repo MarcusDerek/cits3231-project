cdf9aa97a40130e86c03075f941da85863200509
Marcus Derek <xxlifewirexx@gmail.com>
2012-10-25 23:20:19 +0800

    Added Storage folder, add SSL Bank

---
+++ SSLBank/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-MacOSX
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-MacOSX
+CND_ARTIFACT_NAME_Debug=sslbank
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-MacOSX/sslbank
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-MacOSX/package
+CND_PACKAGE_NAME_Debug=sslbank.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/sslbank.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-MacOSX
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-MacOSX
+CND_ARTIFACT_NAME_Release=sslbank
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-MacOSX/sslbank
+CND_PACKAGE_DIR_Release=dist/Release/GNU-MacOSX/package
+CND_PACKAGE_NAME_Release=sslbank.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-MacOSX/package/sslbank.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
